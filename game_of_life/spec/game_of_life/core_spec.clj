f5393f98473235e1a00b245db4f1666f7063c622
Philip Corliss <pcorliss@50projects.com>
2014-11-18 16:47:08 -0600

    Basic cell survival implemented

--- game_of_life/spec/game_of_life/core_spec.clj
+++ game_of_life/spec/game_of_life/core_spec.clj
@@ -11,21 +11,17 @@
     (should-not false)))
 
 (describe "GameOfLife"
-  (it "1 is 1"
-    (should (= 1 1)))
-
-  ;Any live cell with less than two live neighbours dies, as if caused by under-population.
   (it "an empty game board returns an empty board"
-    (should (= (tick ) [] )))
+    (should= [] (tick) ))
 
   (it "a single cell dies with no neighbors"
-    (should (= (tick {:x 1 :y 1} ) [] )))
+    (should= [] (tick {:x 1 :y 1} )))
 
   (it "a single cell dies with one neighbor"
-    (should (= (tick {:x 1 :y 1} {:x 0 :y 0} ) [] )))
+    (should= [] (tick {:x 1 :y 1} {:x 0 :y 0} )))
 
-  ;(it "a single cell lives with two neighbors"
-    ;(should (= (tick {:x 1 :y 1} {:x 0 :y 0} {:x 2 :y 2} ) [{:x 1 :y 1}] )))
+  (it "a single cell lives with two neighbors"
+    (should= [{:x 1 :y 1}] (tick {:x 1 :y 1} {:x 0 :y 0} {:x 2 :y 2})))
 
   (it "cell neighbors"
     (should (neighbor? {:x  1 :y -1} {:x 0 :y 0}))
@@ -41,6 +37,8 @@
     (should-not (neighbor? {:x  0 :y  0} {:x 0 :y 0}))
     (should-not (neighbor? {:x  0 :y  0} {:x 2 :y 0}))
     (should-not (neighbor? {:x  0 :y  0} {:x 0 :y 2})))
+
+  ;Any live cell with less than two live neighbours dies, as if caused by under-population.
   ;Any live cell with two or three live neighbours lives on to the next generation.
   ;Any live cell with more than three live neighbours dies, as if by overcrowding.
   ;Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
--- game_of_life/src/game_of_life/core.clj
+++ game_of_life/src/game_of_life/core.clj
@@ -7,28 +7,52 @@
   [& args]
   (println "Hello, World!"))
 
-(defn tick
-  "Increments the game state"
-  [& cells]
-  (combo/combinations cells 2)
-  [])
+(defn map-get-int
+  "Gets a value from a map defaults to zero"
+  [our-key our-map]
+  (or (-> our-key our-map) 0))
 
 (defn neighbor?
   "Determines if two cells are neighbors"
   [cell1 cell2]
-  (let  [{x1 :x y1 :y} cell1]
-  (let  [{x2 :x y2 :y} cell2]
+  (let [{x1 :x y1 :y} cell1]
+  (let [{x2 :x y2 :y} cell2]
     (and
-      (not
-        (and
-          (= x1 x2)
-          (= y1 y2)))
+      (not (= cell1 cell2))
       (<= (Math/abs (- x1 x2)) 1)
       (<= (Math/abs (- y1 y2)) 1)))))
+
+(defn neighbor-counter
+  "takes a hash keyed by coords and increments based on neighbor pairs"
+  [acc cells]
+  (let [[cell1 cell2] cells]
+  (if (neighbor? cell1 cell2)
+    (merge acc
+      {cell1 (+ 1 (map-get-int cell1 acc))}
+      {cell2 (+ 1 (map-get-int cell2 acc))})
+    acc)))
+
+(defn survive?
+  "applies game of life rules to a cell"
+  [cell]
+  (>= (val cell) 2 ))
+
+(defn tick
+  "Increments the game state"
+  [& cells]
+  (or
+    (keys
+      (filter
+        survive?
+        (reduce
+          neighbor-counter
+          {}
+          (or (combo/combinations cells 2) [] ))))
+    ()))
 
 ;; each cell combination
 ;; test if a cell is a neighbor of another cell
 ;; if it is increment a counter
 ;; iterate through counter hash
 ;; if < 2 it dies
-;; if > 2 it lives
+;; if >= 2 it lives
