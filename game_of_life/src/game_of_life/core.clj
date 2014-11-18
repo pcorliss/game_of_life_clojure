634f7751bf3d43974eb7f78f482b9443256a7fa7
Philip Corliss <pcorliss@50projects.com>
2014-11-18 12:00:50 -0600

    Basic clojure function, some passing tests around empty games.

--- game_of_life/src/game_of_life/core.clj
+++ game_of_life/src/game_of_life/core.clj
@@ -5,3 +5,16 @@
   "I don't do a whole lot ... yet."
   [& args]
   (println "Hello, World!"))
+
+(defn tick
+  "Increments the game state"
+  [& cells]
+  [])
+
+
+;; each cell combination
+;; test if a cell is a neighbor of another cell
+;; if it is increment a counter
+;; iterate through counter hash
+;; if < 2 it dies
+;; if > 2 it lives
--- game_of_life/test/game_of_life/core_test.clj
+++ game_of_life/test/game_of_life/core_test.clj
@@ -2,6 +2,24 @@
   (:require [clojure.test :refer :all]
             [game-of-life.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest game-of-life
+  (testing "1 is 1"
+    (is (= 1 1)))
+
+  ;Any live cell with less than two live neighbours dies, as if caused by under-population.
+  (testing "an empty game board returns an empty board"
+    (is (= (tick ) [] )))
+
+  (testing "a single cell dies with no neighbors"
+    (is (= (tick {:x 1 :y 1} ) [] )))
+
+  (testing "a single cell dies with one neighbor"
+    (is (= (tick {:x 1 :y 1} {:x 0 :y 0} ) [] )))
+
+  (testing "a single cell lives with two neighbors"
+    (is (= (tick {:x 1 :y 1} {:x 0 :y 0} {:x 2 :y 2} ) [{:x 1 :y 1}] )))
+
+  ;Any live cell with two or three live neighbours lives on to the next generation.
+  ;Any live cell with more than three live neighbours dies, as if by overcrowding.
+  ;Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
+  )
